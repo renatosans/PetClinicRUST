c92032c86cc59cd60fc3ceff873a9251b247d51b
renatosans <renato@datacopy.com.br>
2023-11-05 12:41:16 -0300

    Ajustes

--- src/schema.rs
+++ src/schema.rs
@@ -38,14 +38,6 @@ diesel::table! {
 }
 
 diesel::table! {
-    users (id) {
-        id -> Int4,
-        name -> Varchar,
-        email -> Varchar,
-    }
-}
-
-diesel::table! {
     vaccination (id) {
         id -> Int4,
         #[max_length = 120]
@@ -72,7 +64,6 @@ diesel::allow_tables_to_appear_in_same_query!(
     pet,
     petowner,
     treatment,
-    users,
     vaccination,
     veterinarian,
 );
