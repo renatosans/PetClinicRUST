6d2e480dece68ad48f29f07ea41677e5e6e17f09
renatosans <renato@datacopy.com.br>
2023-11-05 12:39:30 -0300

    Inicialização do ORM diesel

---
+++ src/schema.rs
@@ -0,0 +1,78 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    pet (id) {
+        id -> Int4,
+        #[max_length = 120]
+        name -> Varchar,
+        #[max_length = 45]
+        breed -> Nullable<Varchar>,
+        age -> Nullable<Int4>,
+        owner -> Int4,
+    }
+}
+
+diesel::table! {
+    petowner (id) {
+        id -> Int4,
+        #[max_length = 120]
+        name -> Varchar,
+        birth_date -> Nullable<Date>,
+        #[max_length = 80]
+        email -> Varchar,
+        #[max_length = 45]
+        phone -> Nullable<Varchar>,
+        #[max_length = 120]
+        address -> Varchar,
+    }
+}
+
+diesel::table! {
+    treatment (id) {
+        id -> Int4,
+        #[max_length = 120]
+        description -> Varchar,
+        pet -> Int4,
+        veterinarian -> Int4,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        name -> Varchar,
+        email -> Varchar,
+    }
+}
+
+diesel::table! {
+    vaccination (id) {
+        id -> Int4,
+        #[max_length = 120]
+        description -> Varchar,
+        pet -> Int4,
+    }
+}
+
+diesel::table! {
+    veterinarian (id) {
+        id -> Int4,
+        #[max_length = 120]
+        name -> Varchar,
+        #[max_length = 75]
+        inscricaoCRMV -> Varchar,
+    }
+}
+
+diesel::joinable!(treatment -> pet (pet));
+diesel::joinable!(treatment -> veterinarian (veterinarian));
+diesel::joinable!(vaccination -> pet (pet));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    pet,
+    petowner,
+    treatment,
+    users,
+    vaccination,
+    veterinarian,
+);
