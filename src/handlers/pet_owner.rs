1aefe7cef431c7d249a9bd3fd903564ead6e7143
renatosans <renato@datacopy.com.br>
2023-11-11 05:22:22 -0300

    Ajustes

--- src/handlers/pet_owner.rs
+++ src/handlers/pet_owner.rs
@@ -5,7 +5,7 @@ use actix_web::{get, post, patch, delete, web, HttpResponse, Error};
 
 
 #[get("/owners/{owner_id}")]
-async fn select(pool: web::Data<PgPool>, car_id: web::Path<i32>) -> Result<HttpResponse, Error> {
+async fn select(pool: web::Data<PgPool>, owner_id: web::Path<i32>) -> Result<HttpResponse, Error> {
     let car = web::block(move || {
         let mut conn = pool.get().unwrap(); // TODO: fix unwrap
         let result: Result<Option<Petowner>, Error> = cars_for_sale.find(car_id.into_inner()).first(&mut conn).optional();
@@ -18,7 +18,7 @@ async fn select(pool: web::Data<PgPool>, car_id: web::Path<i32>) -> Result<HttpR
 }
 
 #[patch("/owners/{owner_id}")]
-async fn update(pool: web::Data<PgPool>, car_id: web::Path<i32>, payload: web::Json<Petowner>) -> Result<HttpResponse, Error> {
+async fn update(pool: web::Data<PgPool>, owner_id: web::Path<i32>, payload: web::Json<Petowner>) -> Result<HttpResponse, Error> {
     let car = web::block(move || {
         let mut conn = pool.get().unwrap(); // TODO: fix unwrap
         let result: Result<usize, Error> = diesel::update(cars_for_sale.find(car_id.into_inner())).set(payload.into_inner()).execute(&mut conn);
@@ -31,14 +31,19 @@ async fn update(pool: web::Data<PgPool>, car_id: web::Path<i32>, payload: web::J
 }
 
 #[delete("/owners/{owner_id}")]
-async fn delete(pool: web::Data<PgPool>, car_id: web::Path<i32>) -> Result<HttpResponse, Error> {
-    let car = web::block(move || {
-        let mut conn = pool.get().unwrap(); // TODO: fix unwrap
-        let result: Result<usize, Error> = diesel::delete(cars_for_sale.find(car_id.into_inner())).execute(&mut conn);
+async fn delete(pool: web::Data<PgPool>, owner_id: web::Path<i32>) -> Result<HttpResponse, Error> {
+    let connPool = pool.get_ref();
+
+    let query_result = web::block(move || {
+
+        let result = sqlx::query!("DELETE FROM petowner WHERE id = $1", owner_id.into_inner())
+        .execute(connPool);
+
         return result;
     })
-    .await?
+    .await
     .map_err(actix_web::error::ErrorInternalServerError)?;
 
-    Ok(HttpResponse::Ok().json(car))
+    let rows_affected = query_result.await.unwrap().rows_affected();
+    Ok(HttpResponse::Ok().json(rows_affected))
 }
