ebcfe3f75360faa93cd87c46dc2fcfa1c1ec9e3a
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-11-22 09:02:07 -0300

    Ajustes

--- database/create_postgres.sql
+++ database/create_postgres.sql
@@ -2,6 +2,13 @@ CREATE DATABASE pet_clinic;
 \c pet_clinic
 
 
+CREATE TABLE public.appointment (
+    id SERIAL,
+    date date,
+    veterinarian integer NOT NULL,
+    petowner integer NOT NULL
+);
+
 CREATE TABLE public.pet (
     id SERIAL,
     name character varying(120) NOT NULL,
