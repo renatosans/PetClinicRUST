f24f1daceb08923f130057f3fd094fe726bb1b27
renatosans <renato@datacopy.com.br>
2023-12-27 17:08:31 -0300

    Ajustes

--- src/repository/veterinarian.rs
+++ src/repository/veterinarian.rs
@@ -1,5 +1,8 @@
 use sqlx::error::Error;
 use sqlx::postgres::PgPool;
+use async_trait::async_trait;
+use crate::repository::repository::Repository;
+use crate::domain::veterinarian::Veterinarian;
 
 
 pub struct VeterinarianRepository {
@@ -11,5 +14,29 @@ impl VeterinarianRepository {
         VeterinarianRepository {
             pool: pool,
         }
+    }
+}
+
+#[async_trait]
+impl Repository<Veterinarian> for VeterinarianRepository {
+    async fn save(&mut self, payload: Veterinarian) -> Result<(), Error> {
+        // TODO: implementar usando sqlx
+        Ok(())
+    }
+
+    async fn get_by_id(&self, id: i32) -> Option<Veterinarian> {
+        let veterinarian = Veterinarian::default();
+        // TODO: implementar usando sqlx
+        Some(veterinarian)
+    }
+
+    async fn remove(&mut self, id: i32) -> Result<(), Error> {
+        // TODO: implementar usando sqlx
+        Ok(())
+    }
+
+    async fn patch(&mut self, id: i32, payload: Veterinarian) -> Result<(), Error> {
+        // TODO: implementar usando sqlx
+        Ok(())
     }
 }
