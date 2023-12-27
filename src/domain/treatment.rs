a5ef9bc8f4a021696ee915daea833c3b81244f25
renatosans <renato@datacopy.com.br>
2023-12-27 14:08:27 -0300

    Ajustes

--- src/domain/treatment.rs
+++ src/domain/treatment.rs
@@ -1,8 +1,16 @@
 use chrono::NaiveDate;
 
+#[derive(Default, Debug)]
 pub struct Treatment {
     pub id: Option<i32>,
     pub description: String,
     pub pet: i32,
     pub veterinarian: i32,
+}
+
+impl Treatment {
+    pub fn validate(&self) -> bool {
+        
+        true
+    }
 }
--- src/domain/veterinarian.rs
+++ src/domain/veterinarian.rs
@@ -1,7 +1,15 @@
 use chrono::NaiveDate;
 
+#[derive(Default, Debug)]
 pub struct Veterinarian {
     pub id: Option<i32>,
     pub name: String,
     pub inscricao_crmv: String,
+}
+
+impl Veterinarian {
+    pub fn validate(&self) -> bool {
+
+        true
+    }
 }
